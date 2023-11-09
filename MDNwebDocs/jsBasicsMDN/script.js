e07f41eb9aabd1198bcf687e7e02be8e326775d0
Magooch <goochmagooch@gmail.com>
2023-11-08 20:49:00 -0700

    Add MDNwebDocs directory

---
+++ MDNwebDocs/jsBasicsMDN/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function createParagraph() {
+      const para = document.createElement("p");
+      para.textContent = "You clicked the button!";
+      document.body.appendChild(para);
+    }
+  
+    const buttons = document.querySelectorAll("button");
+  
+    for (const button of buttons) {
+      button.addEventListener("click", createParagraph);
+    }
+  });
+
+  const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
