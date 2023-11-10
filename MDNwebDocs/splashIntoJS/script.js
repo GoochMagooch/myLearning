79df280f615d27afc8b704c009078973b534822f
Magooch <goochmagooch@gmail.com>
2023-11-09 19:09:55 -0700

    Add functions and constants to store user data

--- MDNwebDocs/splashIntoJS/script.js
+++ MDNwebDocs/splashIntoJS/script.js
@@ -1,14 +1,45 @@
 // variable of raNum, given the value of a random number between 1 - 100
-const raNum = Math.floor(Math.random() * 100 + 1);
+let raNum = Math.floor(Math.random() * 100) + 1;
 
-// skeleton of function that takes raNum and runs it against user input
-// to determine if the user input matches the value of raNum
-function userInput() {
-if (guess < raNum) {
-    console.log("You guessed lower than the number, try again!");
-} else if (guess > raNum) {
-    console.log("You guessed higher than the number, try again!");
-} else (guess === raNum) {
-    console.log("You guessed the number, congratulations!");
-}
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guess = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+// Function that checks the users guess and runs it through a series of
+// conditional statements, resulting in one of many results
+function checkGuess() {
+    const userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = "Previous guesses: ";
+    }
+    guesses.textContent = `${guesses.textContent} ${userGuess}`;
+
+    if (guess === raNum) {
+        lastResult.textContent = "You guessed the number, congratulations!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = "Game Over - womp";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else {
+        lastResult.textContent = "nop!";
+        lastResult.style.backgroundColor = "red";
+        if (guess < raNum) {
+            lowOrHi.textContent = "You guessed less than the number, try again!";
+        } else if (guess > raNum) {
+            lowOrHi.textContent = "You guessed higher than the number, try again!";
+        }
+    }
+
+guessCount++;
+guessField.value = "";
+guessField.focus();
 }
