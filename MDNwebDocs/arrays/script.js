1bc4488be03c6e1d08bbd93040e1882e63c1e64e
Magooch <goochmagooch@gmail.com>
2023-11-19 20:16:44 -0700

    Attempt a series of instructions. Lots of errors

--- MDNwebDocs/arrays/script.js
+++ MDNwebDocs/arrays/script.js
@@ -7,7 +7,7 @@ ${shopping[0][1]}`;
 console.log(shopping);
 */
 
-const birds = ['Parrot', 'Falcon', 'Owl'];
+/*const birds = ['Parrot', 'Falcon', 'Owl'];
 
 for (const bird of birds) {
     console.log(bird);
@@ -25,3 +25,44 @@ const dogNames = ["Rocket", "Flash", "Bella", "Slugger"];
 const dogNamesUpdated = dogNames.join(', ');
 console.log(dogNamesUpdated);
 console.log(dogNamesUpdated.length);
+*/
+
+const list = document.querySelector('.output ul');
+const totalBox = document.querySelector('.output p');
+let total = 0;
+list.innerHTML = '';
+totalBox.textContent = '';
+// number 1
+                'Underpants:6.99'
+                'Socks:5.99'
+                'T-shirt:14.99'
+                'Trousers:31.99'
+                'Shoes:23.99';
+const products = [{Underpants:6.99}, {Socks:5.99}, {T-shirt:14.99}, 
+{Trousers:31.99}, {Shoes:23.99}];
+
+// number 2
+const (for product of products) {
+	console.log(product);
+}
+  // number 3
+products.split(':');
+const (for product of products) {
+    +[1];
+}
+  // number 4
+  
+const (for product of products) {
+    products.total[1];
+}
+
+  // number 5
+  let itemText = 0;
+
+  const listItem = document.createElement('li');
+  listItem.textContent = itemText;
+  list.appendChild(listItem);
+
+// number 6
+
+totalBox.textContent = 'Total: $' + total.toFixed(2);
