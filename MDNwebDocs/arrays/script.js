a00f7e4a1c3569920e5fa15480ccdb67847a68b0
Magooch <goochmagooch@gmail.com>
2023-11-19 19:50:31 -0700

    Add various coding blocks for practice

---
+++ MDNwebDocs/arrays/script.js
@@ -0,0 +1,14 @@
+/*const shopping = [{'bread' : 3.50}, {'milk' : 2}, {'cheese' : 4}, 
+{'hummus' : 2.25}, {'noodles' : 1}];
+
+const myBudget = `The price of my grocery list is a total of
+${shopping[0][1]}`;
+
+console.log(shopping);
+*/
+
+const birds = ['Parrot', 'Falcon', 'Owl'];
+
+for (const bird of birds) {
+    console.log(bird);
+}
