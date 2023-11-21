5f97d49ab0a84a7455093a3e98d39209d6921c9a
Magooch <goochmagooch@gmail.com>
2023-11-20 22:24:42 -0700

    Trying my hardest :(

---
+++ MDNwebDocs/sillyStory/script.js
@@ -0,0 +1,11 @@
+const randomize = document.querySelector('.randomize');
+const customname = document.querySelector('.customname');
+const para = 'It was 94 fahrenheit outside, so Bob went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
+function create() {
+    (value.customname == true) {
+        para.replace('Bob', ${customname});
+    } else {
+        return 0
+    }
+}
