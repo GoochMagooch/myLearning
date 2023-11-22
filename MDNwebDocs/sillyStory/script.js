dc322e596eb52e3dbfc42c895886e97483108003
Magooch <goochmagooch@gmail.com>
2023-11-21 17:49:59 -0700

    Create variables for input box, button and paragraph where story will be inserted

--- MDNwebDocs/sillyStory/script.js
+++ MDNwebDocs/sillyStory/script.js
@@ -1,11 +1,8 @@
 const randomize = document.querySelector('.randomize');
-const customname = document.querySelector('.customname');
-const para = 'It was 94 fahrenheit outside, so Bob went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+const customname = document.querySelector('#customname');
+const story = document.querySelector('.story')
 
-function create() {
-    (value.customname == true) {
-        para.replace('Bob', ${customname});
-    } else {
-        return 0
-    }
-}
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+  }
