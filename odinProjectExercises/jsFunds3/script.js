903309417a1698046086c2e01a5a3580ba731596
Magooch <goochmagooch@gmail.com>
2023-11-23 13:29:53 -0700

    Change remaining functions into arrow functions

--- odinProjectExercises/jsFunds3/script.js
+++ odinProjectExercises/jsFunds3/script.js
@@ -1,25 +1,15 @@
 /* adds 7 to a given parameter */
 let add7 = num => num + 7;
-
 console.log(add7(62))
 
 /* returns the product of two number parameters */
-function multiply(num1,num2) {
-    return num1 * num2
-}
-
+let multiply = (num1,num2) => num1 * num2;
 console.log(multiply(70,6))
 
 /* capitalizes the first letter of a string parameter */
-function capitalize(string) {
-    return string.charAt().toUpperCase() + string.slice(1)
-}
-
+let capitalize = (string) => string.charAt().toUpperCase() + string.slice(1);
 console.log(capitalize('this is my boomstick!'))
 
 /* returns the last letter of a string parameter */
-function lastLetter(string) {
-    return string.slice(-1)
-}
-
+let lastLetter = (string) => string.slice(-1);
 console.log(lastLetter('the last letter should be returned'))
