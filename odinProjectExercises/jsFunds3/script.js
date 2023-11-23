c71b48cd552fa05cdb5c82395b9b80aa7e69fa8c
Magooch <goochmagooch@gmail.com>
2023-11-23 13:02:44 -0700

    Change comments for other functions

--- odinProjectExercises/jsFunds3/script.js
+++ odinProjectExercises/jsFunds3/script.js
@@ -1,20 +1,22 @@
-/* Function that adds 7 to a given parameter */
+/* adds 7 to a given parameter */
 function add7(num) {
     return num + 7;
 };
 
 console.log(add7(12));
 
-/* Function that takes 2 numbers and returns their product */
+/* returns the product of two number parameters */
 function multiply(num1,num2) {
     return num1 * num2;
 };
 
 console.log(multiply(4,9));
 
-/* Function that capitalizes the first letter of a string */
+/* capitalizes the first letter of a string parameter */
 function capitalize(string) {
     return string.charAt().toUpperCase() + string.slice(1);
 }
 
 console.log(capitalize('this is my boomstick!'));
+
+/* returns the last letter of a string parameter */
