7bdf4ba937d7e5ac5fc86e0d16969a5aeade09be
Magooch <goochmagooch@gmail.com>
2023-11-23 12:45:21 -0700

    Create function that multiplies 2 numbers

--- odinProjectExercises/jsFunds3/script.js
+++ odinProjectExercises/jsFunds3/script.js
@@ -1,7 +1,13 @@
-/*Function that adds 7 to a given parameter*/
+/* Function that adds 7 to a given parameter */
 function add7(num) {
     console.log(num + 7);
 };
 
 add7(12);
 
+/* Function that takes 2 numbers and returns their product */
+function multiply(num1,num2) {
+    console.log(num1 * num2);
+};
+
+multiply(4,9);
