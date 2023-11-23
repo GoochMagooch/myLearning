2497b8b47d53a58305ae0924bdbe4b0e7953262b
Magooch <goochmagooch@gmail.com>
2023-11-23 12:57:52 -0700

    Create function that capitalizes the first letter of a string

--- odinProjectExercises/jsFunds3/script.js
+++ odinProjectExercises/jsFunds3/script.js
@@ -1,13 +1,19 @@
 /* Function that adds 7 to a given parameter */
 function add7(num) {
-    console.log(num + 7);
+    return num + 7;
 };
 
-add7(12);
+console.log(add7(12));
 
 /* Function that takes 2 numbers and returns their product */
 function multiply(num1,num2) {
-    console.log(num1 * num2);
+    return num1 * num2;
 };
 
-multiply(4,9);
+console.log(multiply(4,9));
+
+function capitalize(string) {
+    return string.charAt().toUpperCase() + string.slice(1);
+}
+
+console.log(capitalize('this is my string'));
