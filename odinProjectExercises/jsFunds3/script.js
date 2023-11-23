6bb0c571a71bb64a9d22c41a967743b36a0d618d
Magooch <goochmagooch@gmail.com>
2023-11-23 13:26:16 -0700

    Omit parentheses from arrow function

--- odinProjectExercises/jsFunds3/script.js
+++ odinProjectExercises/jsFunds3/script.js
@@ -1,14 +1,14 @@
 /* adds 7 to a given parameter */
-let add7 = (num) => num + 7;
+let add7 = num => num + 7;
 
-console.log(add7(12))
+console.log(add7(62))
 
 /* returns the product of two number parameters */
 function multiply(num1,num2) {
     return num1 * num2
 }
 
-console.log(multiply(4,9))
+console.log(multiply(70,6))
 
 /* capitalizes the first letter of a string parameter */
 function capitalize(string) {
