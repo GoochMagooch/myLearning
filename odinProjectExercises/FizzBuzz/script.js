5314bc160c817629795b71f6be6601727e2fd178
Magooch <goochmagooch@gmail.com>
2023-11-24 15:07:31 -0700

    Write erroneous for loop that is supposed to envoke FizzBuzz goal

--- odinProjectExercises/FizzBuzz/script.js
+++ odinProjectExercises/FizzBuzz/script.js
@@ -3,17 +3,22 @@ const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 
 function choose() {
-    if (num.value == 0 || num.value > 100) {
-        output.textContent = 'choose a number that is 1 through 100';
+    if (num.value >= 1 && num.value <= 100) {
+        for (let i = 0; i < num; i++) {
+            output.textContent += i;
+          } if (i % 3 == 0) {
+              output.textContent = "Fizz"
+          } else if (i % 5 == 0) {
+              output.textContent = "Buzz"
+          } else if (i % 3 == 0 && i % 5 == 0) {
+              output.textContent = "FizzBuzz"
+          } else {
+              output.textContent = i
+          }
     } else {
-        output.textContent = 'so far your program is working';
+        output.textContent = 'choose a number that is 1 through 100';
     }
+
 }
 
 button.addEventListener('click', choose);
-
-/*
-If num === 0 || > 100 => alert(the number must be 1 through 100)
-else => rest of the function
-*/
-
