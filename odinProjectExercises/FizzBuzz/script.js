43e0394d1b3b9c8c83c6f54df0e322fc1d281630
Magooch <goochmagooch@gmail.com>
2023-11-24 14:13:14 -0700

    Check if num.value is 1 through 100

--- odinProjectExercises/FizzBuzz/script.js
+++ odinProjectExercises/FizzBuzz/script.js
@@ -1,4 +1,19 @@
-const input = document.querySelector('.input');
+const num = document.querySelector('.num');
 const button = document.querySelector('.button');
 const output = document.querySelector('.output');
+
+function choose() {
+    if (num.value == 0 || num.value > 100) {
+        output.textContent = 'choose a number that is 1 through 100';
+    } else {
+        output.textContent = 'so far your program is working';
+    }
+}
+
+button.addEventListener('click', choose);
+
+/*
+If num === 0 || > 100 => alert(the number must be 1 through 100)
+else => rest of the function
+*/
 
