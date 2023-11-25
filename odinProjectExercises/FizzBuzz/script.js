52bc569817fbfb23f30019eda0349072f205878e
Magooch <goochmagooch@gmail.com>
2023-11-25 12:52:57 -0700

    Switch around conditionals and switch to console program

--- odinProjectExercises/FizzBuzz/script.js
+++ odinProjectExercises/FizzBuzz/script.js
@@ -4,21 +4,22 @@ const output = document.querySelector('.output');
 
 function choose() {
     if (num.value >= 1 && num.value <= 100) {
-        for (let i = 0; i < num.value; i++){
-            output.textContent += i;
-          } if (i % 3 === 0) {
-              output.textContent = "Fizz"
-          } else if (i % 5 === 0) {
-              output.textContent = "Buzz"
-          } else if (i % 3 === 0 && i % 5 == 0) {
-              output.textContent = "FizzBuzz"
-          } else {
-              output.textContent = i
+        for (let i = 1; i <= num.value; i++){
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log('Fizz')
+            } else if (i % 5 === 0) {
+                console.log('Buzz')
+            } else if (i % 3 === 0) {
+                console.log('FizzBuzz')
+            } else {
+                console.log(i)
+            }
           }
     } else {
-        output.textContent = 'choose a number that is 1 through 100';
+        alert('choose a number that is 1 through 100');
     }
 
 }
 
 button.addEventListener('click', choose);
+
