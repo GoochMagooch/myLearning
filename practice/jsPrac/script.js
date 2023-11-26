7b8b0009985c48623e4c41be10dd85e6d621c5d4
Magooch <goochmagooch@gmail.com>
2023-11-26 13:45:37 -0700

    Mess with for loop and first condition

--- practice/jsPrac/script.js
+++ practice/jsPrac/script.js
@@ -2,8 +2,8 @@ const button = document.querySelector('.button');
 const num = document.querySelector('.num');
 
 function oddEven() {
-    if (num.value == number) {
-        for (i; i <= num.value; i++) {
+    if (num.value == typeof(Number)) {
+        for (i = 1; i <= num.value; i++) {
             if (num.value % 2 === 0) {
                 console.log('even');
             } else {
@@ -16,3 +16,17 @@ function oddEven() {
 }
 
 button.addEventListener('click', oddEven);
+
+/*
+Next steps:
+When the first if statement is checking even for a number between 1 and 100
+and the number I enter is a number between 1 and 100, the console does not
+spit out an 'even' or 'odd' for each number. It only spits out 1 'even' or
+1 'odd', essentially saying "yes there are even numbers and odd numbers",
+but it's not spitting out every instance of a number.
+I think something is wrong with the for loop.
+
+Also I need to figure out how to write the first condition as "if ANY number
+is entered, do the following". Not just asking for a number in a certain 
+range
+*/
