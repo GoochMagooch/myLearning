3489b4ff206e350634fbbabaf4cb02cee8b5672b
Magooch <goochmagooch@gmail.com>
2023-11-26 13:30:22 -0700

    Create script to perform objective

--- practice/jsPrac/script.js
+++ practice/jsPrac/script.js
@@ -1,2 +1,18 @@
 const button = document.querySelector('.button');
 const num = document.querySelector('.num');
+
+function oddEven() {
+    if (num.value == number) {
+        for (i; i <= num.value; i++) {
+            if (num.value % 2 === 0) {
+                console.log('even');
+            } else {
+                console.log('odd');
+            }
+        }
+    } else {
+        alert('please enter a number');
+    }
+}
+
+button.addEventListener('click', oddEven);
