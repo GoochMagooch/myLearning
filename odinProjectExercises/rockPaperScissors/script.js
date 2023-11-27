2daea91988b0a557533cdde234e91beb980c4e87
Magooch <goochmagooch@gmail.com>
2023-11-27 16:21:38 -0700

    Begin script for rock paper scissors

---
+++ odinProjectExercises/rockPaperScissors/script.js
@@ -0,0 +1,13 @@
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors'
+
+function game(choice) {
+    if (choice == rock) {
+        console.log()
+    } else if (choice == paper) {
+        console.log()
+    } else {
+        console.log()
+    }
+}
