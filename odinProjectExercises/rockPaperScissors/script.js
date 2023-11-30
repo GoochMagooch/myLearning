378a43740448212b52c85c29747704ca384564de
Magooch <goochmagooch@gmail.com>
2023-11-29 22:00:43 -0700

    Write out script for rock paper scissors

--- odinProjectExercises/rockPaperScissors/script.js
+++ odinProjectExercises/rockPaperScissors/script.js
@@ -1,13 +1,30 @@
+const button = document.querySelector('.button');
+const move = document.querySelector('.move');
+
 const rock = 'rock';
 const paper = 'paper';
-const scissors = 'scissors'
+const scissors = 'scissors';
+const index = Math.floor(Math.random() * 3);
+const choices = [rock, paper, scissors];
 
-function game(choice) {
-    if (choice == rock) {
-        console.log()
-    } else if (choice == paper) {
-        console.log()
+function game() {
+    const computer = choices[index];
+    console.log('computer has chosen: ' + computer);
+    if (move.value == rock && computer == paper) {
+        console.log('computer wins!')
+    } else if (move.value == rock && computer == scissors) {
+        console.log('user wins!')
+    } else if (move.value == paper && computer == rock) {
+        console.log('user wins!')
+    } else if (move.value == paper && computer == scissors) {
+        console.log('computer wins!')
+    } else if (move.value == scissors && computer == rock) {
+        console.log('computer wins')
+    } else if (move.value == scissors && computer == paper) {
+        console.log('user wins!')
     } else {
-        console.log()
+        console.log('stalemate. choose again...')
     }
 }
+
+button.addEventListener('click', game);
