ba0228383a89fa4afcada4b633c1c25d01d67b23
Magooch <goochmagooch@gmail.com>
2023-11-30 07:31:10 -0700

    Set condition if anything other than rock paper or scissors is inputted

--- odinProjectExercises/rockPaperScissors/script.js
+++ odinProjectExercises/rockPaperScissors/script.js
@@ -9,21 +9,29 @@ const choices = [rock, paper, scissors];
 
 function game() {
     const computer = choices[index];
-    console.log('computer has chosen: ' + computer);
     if (move.value == rock && computer == paper) {
+        console.log('computer has chosen: ' + computer);
         console.log('computer wins!')
     } else if (move.value == rock && computer == scissors) {
+        console.log('computer has chosen: ' + computer);
         console.log('user wins!')
     } else if (move.value == paper && computer == rock) {
+        console.log('computer has chosen: ' + computer);
         console.log('user wins!')
     } else if (move.value == paper && computer == scissors) {
+        console.log('computer has chosen: ' + computer);
         console.log('computer wins!')
     } else if (move.value == scissors && computer == rock) {
+        console.log('computer has chosen: ' + computer);
         console.log('computer wins')
     } else if (move.value == scissors && computer == paper) {
+        console.log('computer has chosen: ' + computer);
         console.log('user wins!')
-    } else {
+    } else if (move.value == rock && computer == rock || move.value == paper && computer == paper || move.value == scissors && computer == scissors) {
+        console.log('computer has chosen: ' + computer);
         console.log('stalemate. choose again...')
+    } else {
+        alert('you must choose between \'rock\' \'paper\' or \'scissors\'')
     }
 }
 
