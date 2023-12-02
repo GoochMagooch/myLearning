f9d82ea91f74a25ddc30c7f62912ba49f9825780
Magooch <goochmagooch@gmail.com>
2023-12-01 17:52:12 -0700

    Convert console.log output to textContent

--- odinProjectExercises/rockPaperScissors/script.js
+++ odinProjectExercises/rockPaperScissors/script.js
@@ -14,26 +14,26 @@ const choices = [rock, paper, scissors];
 function game() {
     const computer = choices[index];
     if (move.value == rock && computer == paper) {
-        console.log('computer has chosen: ' + computer);
-        console.log('computer wins!')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        compWin.textContent = 'computer wins!';
     } else if (move.value == rock && computer == scissors) {
-        console.log('computer has chosen: ' + computer);
-        console.log('user wins!')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        userWin.textContent = 'user wins!';
     } else if (move.value == paper && computer == rock) {
-        console.log('computer has chosen: ' + computer);
-        console.log('user wins!')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        userWin.textContent = 'user wins!';
     } else if (move.value == paper && computer == scissors) {
-        console.log('computer has chosen: ' + computer);
-        console.log('computer wins!')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        compWin.textContent = 'computer wins!';
     } else if (move.value == scissors && computer == rock) {
-        console.log('computer has chosen: ' + computer);
-        console.log('computer wins')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        compWin.textContent = 'computer wins!';
     } else if (move.value == scissors && computer == paper) {
-        console.log('computer has chosen: ' + computer);
-        console.log('user wins!')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        userWin.textContent = 'user wins!';
     } else if (move.value == rock && computer == rock || move.value == paper && computer == paper || move.value == scissors && computer == scissors) {
-        console.log('computer has chosen: ' + computer);
-        console.log('stalemate. choose again...')
+        compChoice.textContent = 'computer has chosen: ' + computer;
+        stalemate.textContent = 'stalemate. choose again...';
     } else {
         alert('you must choose between \'rock\' \'paper\' or \'scissors\'')
     }
@@ -41,9 +41,3 @@ function game() {
 
 button.addEventListener('click', game);
 
-function renewGame() {
-    location.reload()
-    game();
-}
-
-button.addEventListener('click', renewGame);
